0082987e375ecb283df20747eb61c80b52a39f71
Andrew Lund <andrew.s.lund@gmail.com>
2023-04-26 11:55:32 -0700

    set up mongodb connection. started on routing

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,20 @@
 const mongoose = require("mongoose");
 
 // 1. Use mongoose to establish a connection to MongoDB
+mongoose.connect('mongodb://localhost:3000/glossary') // note: update to process.env once working
 // 2. Set up any schema and models needed by the app
+const glossarySchema = new mongoose.Schema({
+  word: {
+    type: String,
+    unique: true
+  },
+  definition: String
+});
+
+const Word =  mongoose.model('Word', glossarySchema);
+
 // 3. Export the models
+module.exports.Word = Word;
+
+
 // 4. Import the models into any modules that need them
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -6,14 +6,15 @@ const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.json());
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+app.get()
+app.post()
+app.put()
+app.delete()
+app.options((req, res) => {
+  res.sendStatus(204);
+})
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
