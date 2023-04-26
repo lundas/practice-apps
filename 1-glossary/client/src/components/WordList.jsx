3c1e4330092bb98389fb33d4c21700ca158e69db
Andrew Lund <andrew.s.lund@gmail.com>
2023-04-26 14:38:48 -0700

    added component files

---
+++ 1-glossary/client/src/components/WordList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import WordListEntry from './WordListEntry';
+
+
+
+
+
+export default WordList;
--- 1-glossary/client/src/index.jsx
+++ 1-glossary/client/src/index.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import { render } from "react-dom";
+import Search from "./components/Search.jsx";
+import AddWords from "./components/AddWords.jsx";
+import WordList from "./components/WordList.jsx";
+
+const App = () => {
+  return (
+    <>
+      <header>
+        <h1>Glossary</h1>
+      </header>
+      <div>Search Form goes here</div>
+      <div>Add Words Form goes here</div>
+      <h2>Word List</h2>
+      <div>Word List Goes Here</div>
+    </>
+  )
+}
+
+
 
 render(
-  <div>
-    <p>Hello, World!</p>
-  </div>,
+  <App />,
   document.getElementById("root")
 );
