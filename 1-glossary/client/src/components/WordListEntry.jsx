ca61d85b3b38073dd80cf0b35b2b05d3bca11711
Andrew Lund <andrew.s.lund@gmail.com>
2023-04-26 17:23:48 -0700

    working an editting entries for put request

--- 1-glossary/client/src/components/WordListEntry.jsx
+++ 1-glossary/client/src/components/WordListEntry.jsx
@@ -3,9 +3,14 @@ import React from "react";
 
 const WordListEntry = ({word, updateWord, deleteWord}) => (
   <div>
-    <h3 data-word={word.word}>{word.word}</h3>
-    <p><em>Definition:</em> {word.definition}</p>
-    <button value="edit">edit</button>
+    <h3 data-word={word.word} contentEditable="false">{word.word}</h3>
+    <em>Definition:  </em>
+    <p data-word={word.word} contentEditable="false">{word.definition}</p>
+    <button value="edit" onClick={(e) => {
+      const domNodes = document.querySelectorAll(`[data-word="${word.word}"`);
+      domNodes.forEach((node) => node.contentEditable = true);
+      document.querySelector(`button.${word.word}`).hidden = false;
+    }}>edit</button>
     <button value="delete" onClick={(e) => {
       deleteWord(word)
         .then(()=>{
@@ -15,6 +20,14 @@ const WordListEntry = ({word, updateWord, deleteWord}) => (
           console.log(`Error deleting ${word.word}:`, err);
         })
     }}>delete</button>
+    <button className={`${word.word} submit`} value="submit" hidden onClick={(e) => {
+      const domNodes = document.querySelectorAll(`[data-word="${word.word}"`);
+      domNodes.forEach((node) => {
+        node.contentEditable = false;
+        console.log(node.innerText);
+      })
+      e.target.hidden = true;
+    }}>submit</button>
   </div>
 )
 
