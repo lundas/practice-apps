2a8a1bf5736cf548978fd439aaac8f4e537cf514
Andrew Lund <andrew.s.lund@gmail.com>
2023-04-29 11:22:11 -0700

    added schema.sql

---
+++ 2-checkout/server/schema.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS checkout;
+
+USE checkout
+
+DROP TABLE IF EXISTS responses;
+
+-- CREATE TABLE responses (
+--   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--   s_id VARCHAR(40) NOT NULL,
+--   name TEXT,
+--   email TEXT,
+--   password TEXT,
+--   address_line1 TEXT,
+--   address_line2 TEXT,
+--   city TEXT,
+--   state TEXT,
+--   zipcode TEXT,
+--   phone TEXT,
+--   ccn TEXT,
+--   exp_date TEXT,
+--   cvv TEXT,
+--   billing_zipcode TEXT,
+--   UNIQUE(s_id)
+-- );
